807d06d46b031671e0991c7c92615608eeea9d8a
Matthew A. Bardin <43657205+mbardin@users.noreply.github.com>
2021-02-02 15:28:44 -0600

    fixed clipper function

--- docs/js/utils.js
+++ docs/js/utils.js
@@ -19,16 +19,15 @@ function random(low, high) {
   //a distance function
 
 function dist(x1, y1, x2, y2) {
-    let deltaX = x2 - x1;
-    let deltaY = y2 - y1;
-    let result = Math.abs(Math.sqrt(deltaX / deltaY));
+    let deltaX = x2 - x1; //change in X
+    let deltaY = y2 - y1; //change in Y
+    let result = Math.abs(Math.sqrt(Math.pow(deltaX, 2) / Math.pow(deltaY, 2))); //distance formula
     return result;
 }
 
 
-  let clip = Math.max(0, Math.min(1, 0.5));
-//a clipping function
-function clipper(min, max) {
-    min = 0;
-    max = 1;
-}  
+//clipper function
+function clippy(value, min = 0, max = 100){
+  let result = Math.max(min, Math.min(value, max));
+  return result;
+}
