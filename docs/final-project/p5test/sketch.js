29d43df2fee29ebd6a97e867a6ddc7846c7ebd31
Matthew A. Bardin <43657205+mbardin@users.noreply.github.com>
2021-03-11 16:29:38 -0600

    leaving and saving for the day

--- docs/final-project/p5test/sketch.js
+++ docs/final-project/p5test/sketch.js
@@ -1,3 +1,10 @@
+let cir = {
+  x: 100,
+  y: 100,
+  w: 100,
+  h: 100
+}
+
 function setup() {
     createCanvas(400, 400);
   }
@@ -5,5 +12,12 @@ function setup() {
   function draw() {
     background(220);
     fill(150,200,10);
-    ellipse(width / 2, height / 2, 50,50);
+    triangle(mouseX, mouseY, 0,0, width, height);
+    ellipse(cir.x, cir.y, cir.w, cir.h);
+  }
+
+  function mousePressed(){
+    cir.x = random(0, 400);
+    console.log(cir.x);
+    cir.w = mouseX;
   }
